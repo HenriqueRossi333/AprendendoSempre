fc359dc9d98414fd8c7c20fb4c797007d711f23c
HenriqueRossi333 <hrodriguescruciakrossi48@gmail.com>
2026-09-23 20:56:21 -0300

    Iniciando o servidor

---
+++ app.js
@@ -0,0 +1,9 @@
+//EXTENSÕES DO PROJETO
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT
+
+app.listen(port, ()=>{
+    console.log(`Servidor rodando na porta ${port}`)
+})
