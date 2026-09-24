d2c5235643253f84d392c720023f24bf00e47d47
HenriqueRossi333 <hrodriguescruciakrossi48@gmail.com>
2026-09-23 21:17:47 -0300

    Conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-//EXTENSÕES DO PROJETO
-require("dotenv").config()
-const express = require("express")
-const app = express()
-const port = process.env.PORT
+require("dotenv").config();
 
-app.listen(port, ()=>{
-    console.log(`Servidor rodando na porta ${port}`)
-})
+const express = require("express");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+
+// Informações
+const port = process.env.PORT;
+const usuario = process.env.DB_USER;
+const senha = process.env.DB_PASS;
+
+// Conexão com o MongoDB
+const uri = `mongodb+srv://${usuario}:${senha}@cluster0.gdowrw2.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri);
+
+client.connect()
+    .then(() => {
+        console.log("Conectado ao banco!");
+
+        app.listen(port, () => {
+            console.log(`Servidor rodando na porta ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Não foi possível conectar ao banco!");
+        console.log(error);
+    });
